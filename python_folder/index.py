67bb51684cf7b9cdd2b330a8635bd50254ec6f5c
otogowilliam <otogowilliam@gmail.com>
2025-11-19 14:06:43 +0100

    Add files via upload

---
+++ python_folder/index.py
@@ -0,0 +1,11 @@
+# Assigment on Creating a Python Directory
+students = {
+    "student1": {"name": "John Doe", "age": 16, "grade": "A"},
+    "student2": {"name": "Mary James", "age": 17, "grade": "B"},
+    "student3": {"name": "Alex Brown", "age":15, "grade": "C"}
+}
+# print all student records
+for key, record in students.items():
+    print(f"{key}: {record}")
+
+
